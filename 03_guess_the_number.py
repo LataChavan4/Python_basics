b2c6d10a9fee3451b0efd014803cdb2ebfcc40e0
LataChavan4 <102905814+LataChavan4@users.noreply.github.com>
2022-11-22 17:08:59 +0530

    guess the number
    1. Random module used to generate random number.
    2. While loop used to repeat the process of guessing until it matches with the random number generated by module.

---
+++ 03_guess_the_number.py
@@ -0,0 +1,16 @@
+import random
+systemno=random.randint(1,100)
+count = 0
+number=None
+while (number!= systemno):
+    count+=1
+    number = int(input("Guess the number: "))
+    if (number==systemno):
+        print("You guessed it correctly")
+    else:
+        if number>systemno:
+            print("You are wrong, its smaller than this number")
+        else:
+            print("You are wrong, its greater than this number")
+    
+print(f"You took {count} attempts to guess the number correctly")
