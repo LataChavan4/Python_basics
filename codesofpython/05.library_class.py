6d16cc48c7a97cd485698931b6da0c0ccd1cd7fc
LataChavan4 <102905814+LataChavan4@users.noreply.github.com>
2022-11-27 21:07:00 +0530

    Update 05.library_class.py

--- codesofpython/05.library_class.py
+++ codesofpython/05.library_class.py
@@ -1,7 +1,9 @@
 class Library:
-    def __init__(self,library_name,book_list):
+    def __init__(self, library_name, book_list):
         self.library_name = library_name
         self.book_list = book_list
+        self.records = {}
+
 
     def display(self):
         print(self.book_list)
@@ -9,21 +11,26 @@ class Library:
     def add(self):
         book = input("Enter the book name: ")
         self.book_list.append(book)
+        print('Here is the list of updated books')
 
     def lend(self):
         book = input("Enter the book name: ")
-        if book not in book_list:
+        if book not in self.book_list:
             print('Sorry the book you want to read is not available here')
         elif book in self.book_list:
-             self.book_list[book] = Name
-             print("This book has been allocated to you")
+            name = input('Enter your name: ')
+            self.book_list.remove(book)
+            self.records[book] = name
+            print(f"This book has been allocated to you {self.records}")
         else:
-            print("The book you want to read is with other user")    
+            print(f"The book you want to read is with {self.records[book]}")    
 
 
     def ret(self):
          book = input("Enter the book name: ")
-         book_list[book] ='Available'
+         self.book_list.append(book)
+         self.records[book] = 'Available'
+         print("Your book has been submitted successfully")
 
 Dyanda_Library = Library('Dyanda_Library', ['Yayati','Mrutunjay','Chava','Champak'])
 while True:
@@ -32,6 +39,7 @@ while True:
         break
     elif action == 'add':
         Dyanda_Library.add()
+        Dyanda_Library.display()
     elif action == 'lend':
         Dyanda_Library.display()
         Dyanda_Library.lend()
@@ -39,7 +47,9 @@ while True:
         Dyanda_Library.ret()
     else:
         print("please enter valid input")
-    
 
+
+    print('Thank You..!! for visiting our library\n')
+    
 
 
