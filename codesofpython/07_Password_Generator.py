651318cf7355f42113c9e7945e3611603f349257
LataChavan4 <102905814+LataChavan4@users.noreply.github.com>
2022-12-08 14:57:33 +0530

    Update 07_Password_Generator.py

--- codesofpython/07_Password_Generator.py
+++ codesofpython/07_Password_Generator.py
@@ -2,10 +2,10 @@ import string
 import random
 
 s = []
-s1 = string.ascii_uppercase
-s2 = string.ascii_lowercase
-s3 = string.punctuation
-s4 = string.digits
+s1 = string.ascii_uppercase   #characters in upppercase
+s2 = string.ascii_lowercase   #characters on lowercase
+s3 = string.punctuation       #Special Characters
+s4 = string.digits            # numbers
 s.extend(s1)
 s.extend(s2)
 s.extend(s3)
