29417a6d7c8e4c799f170616016d741686e51d9c
LataChavan4 <102905814+LataChavan4@users.noreply.github.com>
2022-12-09 13:58:03 +0530

    Update global_variable.py

--- codesofpython/global_variable.py
+++ codesofpython/global_variable.py
@@ -6,3 +6,18 @@ def func():
 func()    # will create local variable with new value i.e. 8
 print(a)  # value of golbal variable will remain same i'e. 24
 
+
+
+#****using global variable****
+
+a = 24
+def func():
+    global a  # use global variable
+    print (a) # will print defined value of global variable i.e 24
+    a = 8
+    print(a)
+
+func()    # will uppdate global variable with new value i.e. 8
+print(a)  # value of golbal variable will remain changed i.e 8 
+
+
