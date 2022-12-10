e019fbcc8080c1ce7e5912de67aaff54455228d6
LataChavan4 <102905814+LataChavan4@users.noreply.github.com>
2022-12-10 17:26:52 +0530

    Update list_comprehension.py

--- codesofpython/list_comprehension.py
+++ codesofpython/list_comprehension.py
@@ -1,3 +1,8 @@
+
+# list comprehension is an elegant way to create list based on existing list can be used to create set and dictionary
+
+
+
 list = [1,2,3,4,5,6,7,8,9]
 even = [item for item in list if item%2==0]
 print(even)
