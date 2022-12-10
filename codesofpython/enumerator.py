a956ae25db391d5a2b1b8d9bf80e1edfe27b9504
LataChavan4 <102905814+LataChavan4@users.noreply.github.com>
2022-12-10 16:43:17 +0530

    Update enumerator.py

--- codesofpython/enumerator.py
+++ codesofpython/enumerator.py
@@ -1,4 +1,14 @@
+
+# enumerator will help us in getting index along with item in the given list 
+
 list = [23, True, 'ganesha', 76.09]
 
 for index, item in enumerate(list):
     print(item, index)
+    
+   
+#output:
+    23 0
+    True 1
+    ganesha 2
+    76.09 3
