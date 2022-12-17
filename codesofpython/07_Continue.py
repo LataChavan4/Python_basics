1ffeb2966e027b48f093bc570c2ed9a72552d985
LataChavan4 <102905814+LataChavan4@users.noreply.github.com>
2022-12-17 23:57:07 +0530

    Add files via upload

---
+++ codesofpython/07_Continue.py
@@ -0,0 +1,17 @@
+# for i in range(10):
+# #     if i==5:
+# #        continue  # It will skip 5 and will print all other values
+
+#     print(i)
+
+for i in range(10):    #will print odd numbers
+
+    if i%2 != 1:
+        continue
+    print(i)
+
+for i in range(10):    #will print even numbers
+
+    if i%2 != 0:
+        continue
+    print(i)
