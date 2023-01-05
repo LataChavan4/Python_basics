e231ba25f9b5c7acca24b415b00edfc0bf009a19
LataChavan4 <102905814+LataChavan4@users.noreply.github.com>
2023-01-05 12:36:42 +0530

    Create math_module.py

---
+++ codesofpython/math_module.py
@@ -0,0 +1,12 @@
+
+import math
+def paint_calc(height, width, cover):
+   num_of_cans = math.ceil((height*width)/cover)
+   print(f"You'll need {num_of_cans} cans of paint.")
+
+  
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
+
