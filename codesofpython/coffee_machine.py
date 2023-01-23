b7e8ad1e508c0509687de9f691460f0619c008f2
LataChavan4 <102905814+LataChavan4@users.noreply.github.com>
2023-01-23 13:34:08 +0530

    Update coffee_machine.py

--- codesofpython/coffee_machine.py
+++ codesofpython/coffee_machine.py
@@ -29,46 +29,62 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
-machine_sate = True
+machine_state = True
+profit = 0
 
-def get_resourcesrequired(choice):
-    ''' check resources required'''
-    coffee_type = MENU[choice]["ingredients"]
-    water = coffee["water"]
-    milk = coffee["milk"]
-    coffee = coffee["coffee"]
-    return f"Required ingredients are water = {coffee.water}, milk = {coffee.milk}, coffee = {coffee.coffee} "
-    if water >= resources["water"] and milk >= resources["milk"] and coffee >= resources["coffe"]:
-        resources["water"] = resources["water"] - water
-        resources["milk"] = resources["milk"] - milk
-        resources["coffee"] = resources["coffee"] - coffee
-        print(f"Here is your {choice}. Enjoy!")
+def resource_check(order_ingredients, resorces):
+    for item in order_ingredients:
+        if order_ingredients[item] > resorces[item]:
+            print(f"​Sorry there is not enough {item}.")
+            return False
+    return True
+
+def process_coins():
+    print("Please insert coins.")
+    quaters = int(input("How many quaters?: "))
+    dimes = int(input("How many dimes?: "))
+    nickels = int(input("How many nickels?: "))
+    penny = int(input("How many penny?: "))
+    total_amount = ((quaters*25) + (dimes*10) + (nickels*5) + (penny))/100
+    return total_amount
+
+
+def check_trascation(coins_received, drink_cost):
+    if coins_received >= drink_cost:
+        change = round(drink_cost - coins_received, 2)
+        print(f"Here is ${change} in change.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+
+def make_coffee(drink_name, order_ingredients):
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name} ☕️. Enjoy!")   
 
 
 
 #TODO1: 1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
+while machine_state:
+    choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
 
-choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    if choice == 'off':
+        machine_state = False
+    elif choice == 'report':
+        print(resources)
+        print(f"profit: {profit}")
+    else:
+        drink = MENU[choice]
+        if resource_check(drink["ingredients"], resources):
+            payment = process_coins()
+            if check_trascation(payment, drink["cost"]):
+                make_coffee(choice,drink["ingredients"])
 
-get_resourcesrequired(choice)
-if choice == 'off':
-    machine_sate = False
-elif choice == 'report':
-    print(resources)
 
-print("Please insert coins.")
-quaters = int(input("How many quaters?: "))
-dimes = int(input("How many dimes?: "))
-nickels = int(input("How many nickels?: "))
-penny = int(input("How many penny?: "))
 
-total_amount = ((quaters*25) + (dimes*10) + (nickels*5) + (penny))/100
-cost = MENU[choice]["cost"]
-if cost > total_amount:
-    print("Sorry that's not enough money. Money refunded.")
-if cost < total_amount:
-    change = (total_amount - cost)
-    print(f"Here is $ {change} in change.")
 
 
 
