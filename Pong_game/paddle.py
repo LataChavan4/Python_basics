abacb493498e334c4b090877410aeae227ee99cd
LataChavan4 <102905814+LataChavan4@users.noreply.github.com>
2023-02-16 18:05:19 +0530

    Create paddle.py

---
+++ Pong_game/paddle.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+r_paddle = (350, 0)
+l_paddle = (-350, 0)
+class Paddle(Turtle):
+
+    def __init__(self,position):
+        super().__init__()
+        self.penup()
+        self.shape('square')
+        self.color('white')
+        self.resizemode('user')
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.speed('fastest')
+        self.goto(position)
+
+    def move_up(self):
+        new_y = (self.ycor()) + 20
+        self.goto(self.xcor(), new_y)
+
+    def move_down(self):
+        new_y = (self.ycor()) - 20
+        self.goto(self.xcor(), new_y)
+
+
+
