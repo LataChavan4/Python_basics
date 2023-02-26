68412d621a09edb12fbfe3e5a837e2c19e8bfe1f
LataChavan4 <102905814+LataChavan4@users.noreply.github.com>
2023-02-26 20:32:34 +0530

    Create list_comprehension_eg.py

---
+++ codesofpython/list_comprehension_eg.py
@@ -0,0 +1,11 @@
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# we can use items of existing list to create new list without using loop ##
+squared_numbers = [num * num for num in numbers]
+print(squared_numbers)
+
+
+# Filter out even numbers using list comprehension ##
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+squared_numbers = [num * num for num in numbers]
+print(squared_numbers)
+
