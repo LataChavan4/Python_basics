996d52cfed40694d4abfec99e2e5fbf8408768dd
LataChavan4 <102905814+LataChavan4@users.noreply.github.com>
2023-03-01 10:44:39 +0530

    Update list_comprehension_eg.py

--- codesofpython/list_comprehension_eg.py
+++ codesofpython/list_comprehension_eg.py
@@ -6,6 +6,6 @@ print(squared_numbers)
 
 # Filter out even numbers using list comprehension ##
 numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-squared_numbers = [num * num for num in numbers]
+squared_numbers = [num for num in numbers if num%2 == 0]
 print(squared_numbers)
 
