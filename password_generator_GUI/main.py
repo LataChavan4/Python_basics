2e143d8a7591726ec930b0a4f1bf7ae32a0a7458
LataChavan4 <102905814+LataChavan4@users.noreply.github.com>
2023-03-12 23:16:44 +0530

    Create main.py

---
+++ password_generator_GUI/main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200)
+logo = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image = logo)
+canvas.pack()
+
+
+
+
+window.mainloop()
